d05d7e7c80f9ba5059068d5711f5ad492a446a43
EkaterinaBulekova <tushka2030@mail.ru>
2018-01-17 09:15:03 +0600

    Firts commit

---
+++ ConsoleFileSystemVisitor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FileSystemVisitorLibrary;
+
+
+namespace ConsoleFileSistemWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var visitor = new FileSystemVisitor();
+            visitor.Started += start_reaction;
+            visitor.Finished += finish_reaction;
+            visitor.Found += find_reaction;
+
+
+            foreach (var item in visitor.FileSystemVisit("d:"))
+            {
+                Console.WriteLine($"{item.Name} - {item.Type} - {item.Date}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void start_reaction(object sender, SystemWalkerEventArgs e)
+        {
+            Console.WriteLine("Start !!!");
+        }
+
+        static void finish_reaction(object sender, SystemWalkerEventArgs e)
+        {
+            Console.WriteLine("Finish !!!");
+        }
+
+        static void find_reaction(object sender, SystemWalkerEventArgs e)
+        {
+            Console.WriteLine("Found {0}!!!", e.Entity.Name);
+        }
+    }
+}
---
+++ FileSystemVisitorLibrary/FileSystemVisitor.cs
@@ -0,0 +1,109 @@
+﻿using FileSystemVisitorLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystemVisitorLibrary
+{
+    public class SystemWalkerEventArgs : EventArgs
+    {
+        public FileSystemItem Entity { get; set; }
+    }
+
+    public class FileSystemVisitor
+    {
+
+        protected virtual void OnStart(SystemWalkerEventArgs e)
+        {
+            Started?.Invoke(this, e);
+        }
+
+        public event EventHandler<SystemWalkerEventArgs> Finished;
+
+        protected virtual void OnFinish(SystemWalkerEventArgs e)
+        {
+            Finished?.Invoke(this, e);
+        }
+
+        public event EventHandler<SystemWalkerEventArgs> Found;
+
+        protected virtual void OnFound(SystemWalkerEventArgs e)
+        {
+            Found?.Invoke(this, e);
+        }
+
+        public event EventHandler<SystemWalkerEventArgs> Started;
+
+        public IEnumerable<FileSystemItem> FileSystemVisit(string rootPath)
+        {
+            if (Directory.Exists(rootPath))
+            {
+                SystemWalkerEventArgs args = new SystemWalkerEventArgs();
+ 
+                OnStart(null);
+                foreach (var item in DyrectoryVisit(rootPath))
+                {
+                    args.Entity = item;
+                    OnFound(args);
+                    yield return item;
+                }
+                OnFinish(null);
+            }
+        }
+
+        public static IEnumerable<FileSystemItem> DyrectoryVisit(string rootPath)
+        {
+            if (Directory.Exists(rootPath))
+            {
+                DirectoryInfo dir = new DirectoryInfo(rootPath);
+                yield return new FileSystemItem()
+                {
+                    Name = dir.Name,
+                    Type = FileSystemItemType.Directory,
+                    Date = dir.LastWriteTime
+                };
+                string[] files;
+                try
+                {
+                    files = Directory.GetFiles(rootPath);
+                }
+                catch
+                {
+                    files = null;
+                }
+
+                if (files != null && files.Length > 0)
+                    foreach (string f in files)
+                    {
+                        FileInfo file = new FileInfo(f);
+                        yield return new FileSystemItem()
+                        {
+                            Name = file.Name,
+                            Type = FileSystemItemType.File,
+                            Date = file.LastWriteTime
+                        };
+                    }
+
+                string[] dirs;
+                try
+                {
+                    dirs = Directory.GetDirectories(rootPath);
+                }
+                catch
+                {
+                    dirs = null;
+                }
+                if (dirs != null && dirs.Length > 0)
+                    foreach (var directory in dirs)
+                    {
+                        foreach (var item in DyrectoryVisit(directory))
+                        {
+                            yield return item;
+                        }
+                    }
+
+            }
+        }
+    }
+
+}
